e25f06210cdf2874a67d12fc1a5c94426e747681
Navid Memari <work.nmemari@gmail.com>
2023-05-10 10:52:11 -0700

    completed comp2

--- components/CompTwo/index.js
+++ components/CompTwo/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
 import styles from './CompTwo.module.css'
+import ReactPlayer from 'react-player'
 
 export default function CompTwo() {
   return (
-    <div>CompTwo</div>
+    <div className={styles.container}>
+      <h1>Promotional Video</h1>\
+      <ReactPlayer className={styles.video} url={'https://www.youtube.com/watch?v=dQw4w9WgXcQ'}/>
+    </div>
   )
 }
