417a45c87f28f71ca7cbb7fed9559081a748437c
Navid Memari <work.nmemari@gmail.com>
2023-05-16 17:11:07 -0700

    comp 4 completed

--- data/characters.js
+++ data/characters.js
@@ -17,36 +17,36 @@ export const characters = [
         title: 'The Zombie',
         char: Char1,
         background: BkG1,
-        description: 'Lorem ipsum dolor sit amet consectetur. Odio non commodo sit blandit facilisis porttitor interdum. Sit sapien pharetra egestas ultrices scelerisque. Duis ut placerat cursus in diam interdum nunc tristique.'
+        description: 'Zombie is a peculiar customer with an insatiable appetite for anything pink and thinky. His constant moans and shuffling footsteps add an eerie ambiance. Despite his decayed appearance and tattered clothing, his hunger for food has remained intact.'
     },
     {
         title: 'The Vampire',
         char: Char2,
         background: BkG2,
-        description: ''
+        description: 'Vampire has an insatiable craving for exquisite flavors, seeking dishes that balance richness and depth. Despite his nocturnal nature, he gracefully navigates the daylight hours to indulge in the culinary offerings of establishments, Even though he’s lived through centuries, even immortal beings appreciate the finer pleasures in life.'
     },
     {
         title: 'The Skeleton',
         char: Char3,
         background: BkG3,
-        description: ''
+        description: 'Skelly is an adorable and mischievous customer who’s palate leans towards playful and vibrant dishes, delighting in colorful presentations and creative combinations. With a small, bony frame and an infectious grin, Skelly is always eager to explore new flavors and culinary adventures. Who said just because you’re dead the adventure ends?'
     },
     {
         title: 'The Ghost',
         char: Char4,
         background: BkG4,
-        description: ''
+        description: 'Ghost is a ghoulish female ghost who resides in depths of the town’s haunted well. Despite her ghostly nature, and spooky demenor, Ghost possesses a delicate and refined palate, favoring dishes that embrace earthly and delicate flavors. Even in the depths of the well, there is good food can be appreciated.'
     },
     {
         title: 'The Fox Spirit',
         char: Char5,
         background: BkG5,
-        description: ''
+        description: 'Kitsune is a cunning and mischievious customer. As a fox spirit, she possesses both human and fox-like characteristics. Kitsune has an peculiar palate, always seeking out new and exotic flavors that challenge her senses. Careful though, she might play a prank or two if you don’t satisfy her tastes. '
     },
     {
         title: 'The Doll',
         char: Char6,
         background: BkG6,
-        description: ''
+        description: 'Dolly is a silent haunted doll who bringing an air of eerie mystery with her. Her porcelain features are delicate and beautiful, but her expression is permanently fixed in a haunting, mournful gaze. Despite her unsettling nature, Rose has a refined taste, preferring dishes that are delicate and subtle in flavor. Even the most haunting of customers can have a discerning palate.'
     }
 ]
