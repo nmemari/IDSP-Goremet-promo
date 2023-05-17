d33a63bbab2a3b97851af439c1c67404b7c7be7f
jnip2 <jnip2@my.bcit.ca>
2023-05-17 10:47:50 -0700

    add comp five

--- pages/index.js
+++ pages/index.js
@@ -20,8 +20,8 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.png" />
       </Head>
+      <NavBar />
       <main className={styles.main}>
-        <NavBar />
         <CompOne />
         <CompTwo />
         <CompThree />
