f22cbe973e84a9b774772088acfab26462c1a6fa
Navid Memari <work.nmemari@gmail.com>
2023-05-18 12:10:24 -0700

    removed slash

--- components/CompTwo/index.js
+++ components/CompTwo/index.js
@@ -5,7 +5,7 @@ import ReactPlayer from 'react-player'
 export default function CompTwo() {
   return (
     <div className={styles.container}>
-      <h1>Promotional Video</h1>\
+      <h1>Promotional Video</h1>
       <ReactPlayer className={styles.video} url={'https://youtu.be/AE4FR9inZoE'}/>
     </div>
   )
